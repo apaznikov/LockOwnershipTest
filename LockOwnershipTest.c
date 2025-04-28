a97a28bd4e69c262c555ec8ea71a8515b1fcb73f
Alexey Paznikov <apaznikov@gmail.com>
2025-04-28 16:46:11 +0800

    Context-sensitive example

--- LockOwnershipTest.c
+++ LockOwnershipTest.c
@@ -11,22 +11,39 @@ pthread_mutex_t Lock2 = PTHREAD_MUTEX_INITIALIZER;
 // void doUnlock() {
 //   pthread_mutex_unlock(&Lock1);
 // }
-//
-// void foo() {
+
+int x = 0;
+
+// int foo() {
+//   pthread_mutex_lock(&Lock2);
 //   doLock();
+//   x++;
 //   doUnlock();
+//   pthread_mutex_unlock(&Lock2);
+//   return x;
 // }
 
-void bar(int x) {
-  pthread_mutex_lock(&Lock1);
-  if (x > 0)
-    pthread_mutex_lock(&Lock2);
-
-  pthread_mutex_unlock(&Lock1);
-
-  if (x > 0)
-    pthread_mutex_unlock(&Lock2);
+int simple() {
+  x++;
+  return x;
 }
+
+void simple_caller2() {
+  pthread_mutex_lock(&Lock2);
+  simple();
+  pthread_mutex_unlock(&Lock2);
+}
+
+// void bar(int x) {
+//   pthread_mutex_lock(&Lock1);
+//   if (x > 0)
+//     pthread_mutex_lock(&Lock2);
+//
+//   pthread_mutex_unlock(&Lock1);
+//
+//   if (x > 0)
+//     pthread_mutex_unlock(&Lock2);
+// }
 
 // int main(void) {
 //     printf("Hello, World!\n");
